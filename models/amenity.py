334272d684fe9739692e6835db6651b38e84c932
Emanuel-js <57919304+Emanuel-js@users.noreply.github.com>
2021-09-19 21:10:27 +0300

    Update amenity.py

--- models/amenity.py
+++ models/amenity.py
@@ -1,21 +1,25 @@
-#!/usr/bin/python
-""" holds class Amenity"""
-import models
-from models.base_model import BaseModel, Base
+#!/usr/bin/python3
+from models.base_model import BaseModel, Base, Table, Column, String
 from os import getenv
-import sqlalchemy
-from sqlalchemy import Column, String
-from sqlalchemy.orm import relationship
+"""
+amenity module
+    contains
+        the Amentiry class inherts from BaseModel and Base
+"""
 
 
 class Amenity(BaseModel, Base):
-    """Representation of Amenity """
-    if models.storage_t == 'db':
+    """
+    The Amenity class
+    """
+    if getenv('HBNB_TYPE_STORAGE', 'fs') == 'db':
         __tablename__ = 'amenities'
         name = Column(String(128), nullable=False)
     else:
         name = ""
 
     def __init__(self, *args, **kwargs):
-        """initializes Amenity"""
+        """
+        initializes class objects. Inherts attributes from parent
+        """
         super().__init__(*args, **kwargs)
